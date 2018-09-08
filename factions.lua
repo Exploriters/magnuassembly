9d3561b6af658fc46067bc0456fc84796820947a
Magnus siiftun1857 Frankline <39226927@qq.com>
2018-09-09 06:24:50 +0800

    First ever

---
+++ factions.lua
@@ -0,0 +1,107 @@
+{
+	1257 = {
+		name=_("_text_faction_Lapismarine"),
+		playable=2,
+		start="1257_Knight",
+		primaries=3,
+		color0=0x5862a0,
+		color1=0x7fc0ff,
+		color2=0x0000ff,
+		aiflags=BAD_AIM|CAUTIOUS|DODGES|FLOCKING|METAMORPHOSIS|SMART_FIRE|SOCIAL|WANDER,
+		thrustSFX=1,
+	},
+	
+	1235 = {
+		name=_("_text_faction_DSSETRNDD"),
+		playable=1,
+		start="1235_Venture",
+		primaries=3,
+		--color0=,
+		--color1=,
+		--color2=,
+		aiflags=DODGES|FLOCKING|METAMORPHOSIS|PEACEFUL|RECKLESS|SMART_FIRE|SOCIAL|TRACTOR_TRANSIENT|WANDER,
+		thrustSFX=1,
+	},
+	
+	1238 = {
+		name=_("_text_faction_Light_Guard"),
+		playable=1,
+		start="1238_Tech_Bomber",
+		primaries=3,
+		--color0=,
+		--color1=,
+		--color2=,
+		aiflags=AGGRESSIVE|DODGES|FLOCKING|METAMORPHOSIS|RECKLESS|RIPPLE_FIRE|SMART_FIRE|SOCIAL|WANDER,
+	},
+	
+	
+	1880 = {
+		name=_("_text_faction_Phage"),
+		playable=2,
+		start="1880_A-1",
+		primaries=3,
+		color0=0xc0ff7f,
+		color1=0x7fff00,
+		color2=0x4fff00,
+		aiflags=CAUTIOUS|DODGES|FLOCKING|METAMORPHOSIS|RECKLESS|SMART_FIRE|SOCIAL|WANDER,
+		thrustSFX=1,
+	},
+
+	1242 = {
+		name=_("_text_faction_Winter_Ender"),
+		playable=1,
+		start="1242_Guard",
+		primaries=3,
+		--color0=,
+		--color1=,
+		--color2=,
+		aiflags=CAUTIOUS|DODGES|METAMORPHOSIS|MUTATE|RECKLESS|RIPPLE_FIRE|SOCIAL|SPREAD_FIRE|WANDER,
+		thrustSFX=1,
+	},
+
+	1243 = {
+		name=_("_text_faction_Outlaw"),
+		playable=1,
+		start="1243_Interpreter",
+		primaries=3,
+		--color0=,
+		--color1=,
+		--color2=,
+		aiflags=AGGRESSIVE|DODGES|FLOCKING|METAMORPHOSIS|MUTATE|RECKLESS|RIPPLE_FIRE|SMART_FIRE|SOCIAL|WANDER,
+		thrustSFX=1,
+	},
+	
+	1881 = {
+		name=_("_text_faction_Null_setter"),
+		start="1881_Nanf",
+		primaries=3,
+		color0=0x800080,
+		color1=0x000000,
+		color2=0xffffff,
+		aiflags=AGGRESSIVE|DODGES|FLOCKING|METAMORPHOSIS|RECKLESS|SOCIAL|WANDER,
+		thrustSFX=1,
+	},
+
+	1443 = {
+		name=_("_text_faction_Stragegic_Brawler"),
+		playable=1,
+		start="1443_Explorer_Lapis",
+		primaries=3,
+		--color0=,
+		--color1=,
+		--color2=,
+		aiflags=CAUTIOUS|DODGES|FLOCKING|METAMORPHOSIS|RECKLESS|SMART_FIRE|SOCIAL|WANDER,
+		thrustSFX=1,
+	},
+
+	1259 = {
+		name=_("_text_faction_Ancient"),
+		primaries=3,
+		--color0=,
+		--color1=,
+		--color2=,
+		aiflags=ATTACK|INACTIVE,
+		thrustSFX=1,
+	},
+
+}
---
+++ regions.lua
@@ -0,0 +1,133 @@
+
+{
+	subregions={
+		{
+			ident=1257001,
+			faction=1257,
+			count=1,
+			position={0.25, 0.25},
+			radius={0.2, 0.2},
+			type=2,
+			fleets={
+				{1257,{{0,715555},{0.8, 71555},{0.33,12500},{1,8000}}},
+--				{1257,{{0,71555},{0.8, 7155},{0.33,1250},{1,800}}},
+--				{1257,{{0,7155},{0.8, 715},{0.33,125},{1,80}}},
+			},
+			fleetCount={10, 15},
+			fleetFraction=1,
+			unique={
+				{
+					"1257_Lapismarine_Homeworld",
+					"1257_Cargoship",
+				},
+			},
+			fleetFraction=0.2,
+			ambient={ 0,1,2 },
+			asteroidDensity={0.25, 0.5},
+			asteroidSize={40, 160},
+			asteroidFlags=PENROSE|SQUARE|HEXAGON|TRIANGLE|OCTAGON,
+		},
+
+		{
+			ident=1257002,
+			faction=1257,
+			count=1,
+			position={0.75, 0.75},
+			radius={0.3, 0.3},
+			type=1,
+			fleets={
+				{1257,{{0,71555},{1,8000}}},
+--				{1257,{{0,7155},{1,800}}},
+--				{1257,{{0,715},{1,80}}},
+			},
+			fleetCount={1, 8},
+			fleetFraction=0.5,
+			unique={
+				{
+					"1257_Lapismarine_Outer_Ring_Checkpoint",
+					"1257_Cargoship",
+				},
+			},
+			uniqueFraction=0.5,
+			ambient={ 0,1,2,-1 },
+			asteroidDensity={0, 0.9},
+			asteroidSize={0, 40},
+			asteroidFlags=PENROSE|SQUARE|HEXAGON|TRIANGLE|OCTAGON,
+		},
+
+		{
+			ident=1257003,
+			faction=1257,
+			count=20,
+			position={0.2, 1.5},
+			radius={0.02, 0.02},
+			type=1,
+			fleets={
+				{1257,{{0,12500},{1,12500}}},
+--				{1257,{{0,8000},{1,8000}}},
+--				{1257,{{0,1250},{1,1250}}},
+--				{1257,{{0,800},{1,800}}},
+--				{1257,{{0,125},{1,125}}},
+			},
+			fleetCount={3, 9},
+			fleetFraction=1,
+			unique={
+				{
+					"1257_Lapismarine_Guardian_Station",
+					"1257_Cargoship",
+				},
+			},
+			uniqueFraction=1,
+			ambient={ 0,1,2,-1 },
+			asteroidDensity={0.25, 0.5},
+			asteroidSize={0, 40},
+			asteroidFlags=PENROSE|SQUARE|HEXAGON|TRIANGLE|OCTAGON,
+		},
+		
+		{
+			ident=1880000,
+			faction=1880,
+			count=25,
+			position={0.2, 1.5},
+			radius={0.02, 0.02},
+			type=2,
+			fleets={
+				{1880, { {0, 80000}, {1, 80000} }},
+--				{1880, { {0, 8000}, {1, 8000} }},
+--				{1880, { {0, 800}, {1, 800} }},
+--				{1880, { {0, 400}, {1, 400} }},
+--				{1880, { {0, 300}, {1, 300} }},
+--				{1880, { {0, 200}, {1, 200} }},
+--				{1880, { {0, 100}, {1, 100} }},
+			},
+			fleetCount={8, 15},
+			fleetFraction=0.8,
+			ambient={ 0 },
+			asteroidDensity={0.1, 0.3},
+			asteroidSize={1, 10},
+			asteroidFlags=PENROSE,
+		},
+		{
+			ident=1881000,
+			faction=1881,
+			count=15,
+			position={0.1, 1.1},
+			radius={0.02, 0.02},
+			type=0,
+			fleets={
+				{1881, { {0, 715555}, {1, 715555} }},
+--				{1881, { {0, 71555}, {1, 71555} }},
+--				{1881, { {0, 7155}, {1, 7155} }},
+--				{1881, { {0, 715}, {1, 715} }},
+			},
+			fleetCount={3, 8},
+			fleetFraction=0.75,
+			unique={
+				{
+					"1881_Gravity_Well",
+				},
+			},
+			uniqueFraction=0.25,
+		},
+	}
+}
