ab4862d09106e87714d8eb907ee527d049ad1c93
Magnus siiftun1857 Frankline <39226927@qq.com>
2018-12-26 11:26:51 +0800

    quick fix

--- blocks/__Tempbox.lua
+++ blocks/__Tempbox.lua
@@ -1,14 +1,14 @@
 fillColor=0x0000ff,\r\n\t\tlineColor=0x7fc0ff,\r\n\t\tpoints=(\-?[0-9]+),\r\n\t\tdurability=3,\r\n\t\tdensity=0.2,\r\n\t\tgrowRate=5,
-#include "blocks/_T_Lapisdevice.lua"\r\n\t\tpoints=\1,
+#include "/blocks/_T_Lapisdevice.lua"\r\n\t\tpoints=\1,
 
 \r\n\t\treplicateBlock=\{(.*?)\r\n\t\t\},
-\r\n\t\treplicateBlock=\{\1\r\n\t\t\t#include "blocks/_T_Lapisfakemissile.lua"\r\n\t\t\},
+\r\n\t\treplicateBlock=\{\1\r\n\t\t\t#include "/blocks/_T_Lapisfakemissile.lua"\r\n\t\t\},
 
 \r\n\t\t\t\t\t\t\treplicateBlock=\{(.*?)\r\n\t\t\t\t\t\t\t\t\},
-\r\n\t\t\t\t\t\t\treplicateBlock=\{\1\r\n\t\t\t\t\t\t\t\t#include "blocks/_T_Lapisfakemissile.lua"\r\n\t\t\t\t\t\t\t\},
+\r\n\t\t\t\t\t\t\treplicateBlock=\{\1\r\n\t\t\t\t\t\t\t\t#include "/blocks/_T_Lapisfakemissile.lua"\r\n\t\t\t\t\t\t\t\},
 
-#include "blocks/_T_Lapisfakemissile.lua"\r\n\t*#include "blocks/_T_Lapisfakemissile.lua"
-#include "blocks/_T_Lapisfakemissile.lua"
+#include "/blocks/_T_Lapisfakemissile.lua"\r\n\t*#include "/blocks/_T_Lapisfakemissile.lua"
+#include "/blocks/_T_Lapisfakemissile.lua"
 
 name=_("
 name=_("_text_Decoy")_("_bl")_("
@@ -16,17 +16,17 @@ name=_("_text_Decoy")_("_bl")_("
 ))))))))))
 
 fillColor=0x5862a0,\r\n\t\tfillColor1=0x7fc0ff,\r\n\t\tlineColor=0x5862a0,\r\n\t\tdurability=23.1,\r\n\t\tdensity=0.15,
-#include "blocks/_T_Lapis_command_style.lua"
+#include "/blocks/_T_Lapis_command_style.lua"
 
 fillColor=0x5862a0,\r\n\t\tlineColor=0x7fc0ff,\r\n\t\tpoints=(\-?[0-9]+),\r\n\t\tdurability=4.65,\r\n\t\tdensity=0.3,\r\n\t\tgrowRate=1.1,\r\n\t\tarmor=25,
-#include "blocks/_T_Lapisdevice_Ancient.lua"\r\n\t\tpoints=\1,
+#include "/blocks/_T_Lapisdevice_Ancient.lua"\r\n\t\tpoints=\1,
 
 fillColor=0xc0c0ff,\r\n\t\tfillColor1=0x7fc0ff,\r\n\t\tlineColor=0x5862a0,\r\n\t\tpoints=(\-?[0-9]+),\r\n\t\tdurability=4,\r\n\t\tdensity=0.15,\r\n\t\tgrowRate=20,
-#include "blocks/_T_Lapishull.lua"\r\n\t\tpoints=\1,
+#include "/blocks/_T_Lapishull.lua"\r\n\t\tpoints=\1,
 
 fillColor=0x0000ff,\r\n\t\tlineColor=0x7fc0ff,\r\n\t\tpoints=(\-?[0-9]+),\r\n\t\tdurability=3,\r\n\t\tdensity=0.2,\r\n\t\tgrowRate=(\-?[0-9]+\.?[0-9]*),
-#include "blocks/_T_Lapisdevice.lua"\r\n\t\tpoints=\1,\r\n\t\tgrowRate=\2,
+#include "/blocks/_T_Lapisdevice.lua"\r\n\t\tpoints=\1,\r\n\t\tgrowRate=\2,
 
 fillColor=0x7f7fff00,\r\n\t\tfillColor1=0x7fc0ff7f,\r\n\t\tlineColor=0x4fff00,\r\n\t\tpoints=(\-?[0-9]+),\r\n\t\tdurability=0.05,\r\n\t\tdensity=0.02,\r\n\t\tgrowRate=153.622,\r\n\t\tmeleeDamage=0,\r\n\t\tarmor=100,
-#include "blocks/_T_Phage_style.lua"\r\n\t\tpoints=\1,
+#include "/blocks/_T_Phage_style.lua"\r\n\t\tpoints=\1,
 
