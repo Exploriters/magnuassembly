943f6790b6c2c3e46f2176d47bd86e011885600e
Magnus siiftun1857 Frankline <39226927@qq.com>
2019-07-21 20:17:31 +0800

    update 3883

--- blocks/_T_3883_BudS.lua
+++ blocks/_T_3883_BudS.lua
@@ -1,4 +1,7 @@
 
 		#include "_T_3883_Bud.lua"
-		fillColor1=0x1c5997,
+		fillColor1=0x4f4f4f,
+	--	fillColor=0x2f2f2f,
+		lineColor=0x2f2f2f,
+		fillColor=0x1c5997,
 		
--- blocks/_T_3883_BudT.lua
+++ blocks/_T_3883_BudT.lua
@@ -1,4 +1,7 @@
 
 		#include "_T_3883_Bud.lua"
+		fillColor=0x4f4f4f,
+	--	fillColor1=0x2f2f2f,
+		lineColor=0x2f2f2f,
 		fillColor1=0x8c5a17,
 		
