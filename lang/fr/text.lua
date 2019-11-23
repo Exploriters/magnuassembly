f5d7d481b2fc9a3d99c46b049082bfdf297c1ca0
Magnus siiftun1857 Frankline <39226927@qq.com>
2019-11-23 13:57:48 +0800

    Update franch

--- lang/fr/text.lua
+++ lang/fr/text.lua
@@ -3,4 +3,20 @@
 
 {
 #include "../source.lua"
+
+	"_text_decoy"="Leurre",
+	"_text_prefix_friendly"="Allié",
+	"_text_hull"="Coque",
+	"_text_seed"="Graine",
+	"_text_plantable_dirt"="Terre",
+	"_text_hard_stone"="Pierre",
+	"_text_generator"="Générateur",
+	"_text_thruster"="Propulseur",
+	"_text_shotgun"="Fusil à Pompe",
+	"_text_rifle"="Fusil",
+	"_text_lapism_royal_rifle"="Fusil Royal",
+	"_text_lapism_fragminecannon_lv1"="Canon à Mine",
+	"_text_lapism_fragminecannon_lv2"="Canon à Grappe de Mine",
+	"_text_lapism_fragminecannon_lv3"="Canon à Champs de Mine",
+	"_text_missile_battery"="Batterie de missile",
 }
